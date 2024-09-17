0f895aebf78be7ac2ef033d802ec0030fb0e80f8
Deewellys <deewellys@gmail.com>
2024-09-17 18:03:15 -0300

    Trabalhando na Sidebar
    Implementei a Sidebar no sistema tentei da import nos icons e mas por algum motivo não está indo vou continuar tentando e ja atualizo aqui novamente

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import './app.css'
 import Home from './pages/home'
 import Login from './pages/Login'
+import Sidebar from './componentes/Sidebar';
 
 export default function App(){
     return(
---
+++ src/componentes/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="logo">
+        <img src="/src/componentes/img/educacaosergipe_logo.png" alt="Logo" />
+      </div>
+      <div className='seduc'>
+        <h1>SEDUC</h1>
+      </div>
+      <ul className="menu">
+        <li>
+          <a href="#">
+            <i className="fa fa-bar-chart"></i>
+            <span>Financeiro</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fas fa-users"></i>
+            <span>Usuários</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fas fa-cog"></i>
+            <span>Configurações</span>
+          </a>
+        </li>
+      </ul>
+      <button className="logout">
+        <i className="fas fa-sign-out-alt"></i>
+        <span>Logout</span>
+      </button>
+    </div>
+  );
+};
+
+export default Sidebar;
