693329fcf79d0ebe671d13f8c8012bf1513f15ba
Deewellys <deewellys@gmail.com>
2024-09-19 17:26:54 -0300

    Feat/ Ajuste na Sidebar e no container
    Agora pode ser exportado corretamente, quanto a responsividade estou vendo se da pra animar direto mantendo ela ou se vai ser necessário trocar pelo react-pro-sidebar.

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,40 +1,48 @@
 import React from 'react';
 import './Sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBarChart, faChartBar, faChartSimple, faCoffee, faCog, faSignOut, faSignOutAlt, faUser, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <div className="logo">
-        <img src="/src/componentes/img/educacaosergipe_logo.png" alt="Logo" />
-      </div>
-      <div className='seduc'>
-        <h1>SEDUC</h1>
-      </div>
-      <ul className="menu">
-        <li>
-          <a href="#">
-            <i className="fa fa-bar-chart"></i>
-            <span>Financeiro</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <i className="fas fa-users"></i>
-            <span>Usuários</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <i className="fas fa-cog"></i>
-            <span>Configurações</span>
-          </a>
-        </li>
-      </ul>
-      <button className="logout">
-        <i className="fas fa-sign-out-alt"></i>
-        <span>Logout</span>
-      </button>
+    <div className="container">
+      <div className="sidebar">
+        <div className="logo">
+            <img src="/src/componentes/img/educacaosergipe_logo.png" alt="Logo" />
+        </div>
+        <div className='seduc'>
+            <h1>SEDUC</h1>
+        </div>
+        <ul className="menu">
+            <li>
+            <a href="#">
+                <i><FontAwesomeIcon icon={faChartSimple}></FontAwesomeIcon></i>
+                <span>Financeiro</span>
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                <i><FontAwesomeIcon icon={faUsers}></FontAwesomeIcon></i>
+                <span>Usuários</span>
+            </a>
+            </li>
+            <li>
+             <a href="#">
+                <i><FontAwesomeIcon icon={faCog}></FontAwesomeIcon></i>
+                <span>Configurações</span>
+            </a>
+            </li>
+        </ul>
+        <button className="logout">
+            <i><FontAwesomeIcon icon={faSignOut}></FontAwesomeIcon></i>
+            <span>Logout</span>
+        </button>
+        </div>
+        <div className="main-content">
+            <div className="home-container">
+            <div className="home">Home</div>
+            </div>
+        </div>
     </div>
   );
 };
