1f0e8bad8fd85183a9912eb488ae26c962587712
Marcos Guilherme <guitto10@outlook.com>
2024-10-09 20:41:17 -0300

    add the table

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,8 +2,28 @@ import './Home.css'
 
 export default function Home () {
     return (
-        <div className="content-container">
-            Façam o seu JSX aqui!
+        <div className="content-container home-container">
+            <div className='home-title'>
+                <h1>BEM-VINDO AO SEDUC</h1>
+            </div>
+            <div className='home-table'>
+                <table>
+                    <div className='table-title'>
+                        <tr>
+                            <div className='th-left'>
+                                <th>Nome da ação</th>
+                            </div>
+                            <div className='th-right'>
+                                <th>Nome</th>
+                                <th>Data</th>
+                            </div>
+                        </tr>
+                    </div>
+                    <tr>
+                        <td>ola</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     )
 }
