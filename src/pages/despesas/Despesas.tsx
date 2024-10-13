401df81e77555cc29a18b966665c0080db2b3303
Deewellys <deewellys@gmail.com>
2024-10-12 21:02:00 -0300

    feat: Olá mundo!

--- src/pages/despesas/Despesas.tsx
+++ src/pages/despesas/Despesas.tsx
@@ -3,8 +3,8 @@ import './Despesas.css';
 export default function Despesas() {
     return (
         <div className="content-container despesas-container">
-            Olá! Eu sou a tela de despesas.
-            {/* Façam o JSX da tela aqui! */}
+            <h1>Olá mundo!</h1>
+        
         </div>
     );
 }
