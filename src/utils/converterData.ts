287f8cfb38af58fa1bb628b954c7b2752c920d54
bytecarlos <109424074+ByteCarlos@users.noreply.github.com>
2024-11-12 00:49:32 -0300

    feat: get logs

---
+++ src/utils/converterData.ts
@@ -0,0 +1,15 @@
+export default function converterData(timestamp: string): string {
+    const date = new Date(timestamp);
+
+    // Formata a data no estilo DD/MM/YYYY
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // getUTCMonth retorna 0 para janeiro
+    const year = date.getUTCFullYear();
+
+    // Formata a hora no estilo HH:MM
+    const hours = String(date.getUTCHours()).padStart(2, '0');
+    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+
+    // Retorna a string formatada
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
+}
